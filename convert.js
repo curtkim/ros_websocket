216c576772654947c183b417e114000d784674e4
curt.k <curt.k@kakaocorp.com>
2020-02-12 15:33:40 +0900

    pointcloud parsing 추가

--- convert.js
+++ convert.js
@@ -13,14 +13,19 @@ function convert_pc() {
     console.log(buf, buf.length)
     const dv = new DataView(buf.buffer)
 
-    for(var i = 0; i < body.msg.width; i += body.msg.point_step){
-        const x = dv.getFloat32(i*body.msg.point_step+0)
-        const y = dv.getFloat32(i*body.msg.point_step+4)
-        const z = dv.getFloat32(i*body.msg.point_step+8)
-        const intensity = dv.getInt8(i*body.msg.point_step+16)
-        const timestamp = dv.getFloat64(i*body.msg.point_step+24)
-        console.log(x.toFixed(2),y.toFixed(2),z.toFixed(2),intensity,timestamp)
+    const littleEndian = !body.msg.is_bigendian
+
+    const writer = fs.createWriteStream('0.xyz')
+
+    for(var i = 0; i < body.msg.width; i++){
+        const x = dv.getFloat32(i*body.msg.point_step+0, littleEndian)
+        const y = dv.getFloat32(i*body.msg.point_step+4, littleEndian)
+        const z = dv.getFloat32(i*body.msg.point_step+8, littleEndian)
+        const intensity = dv.getInt8(i*body.msg.point_step+16, littleEndian)
+        const timestamp = dv.getFloat64(i*body.msg.point_step+24, littleEndian)
+        writer.write(x.toFixed(2) + " " + y.toFixed(2) + " " + z.toFixed(2) + " " + intensity + "\n") //, timestamp
     }
+    writer.end()
     //fs.writeFileSync("0.pcd", buf)
 }
 
